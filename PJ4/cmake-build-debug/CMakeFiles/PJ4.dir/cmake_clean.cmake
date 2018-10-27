641f4f10cd4fcbdd50cbf81cdc2f1ebbaf0a92ac
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-10-27 16:02:59 -0500

    project done, accidently pushed to wrong repo

---
+++ PJ4/cmake-build-debug/CMakeFiles/PJ4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PJ4.dir/main.c.o"
+  "PJ4.pdb"
+  "PJ4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PJ4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
