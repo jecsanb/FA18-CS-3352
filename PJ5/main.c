9527c6846d6faefe5f92970afae31c847cf68547
Jecsan Blanco <jblancolicano1@buffs.wtamu.edu>
2018-11-08 22:33:37 -0600

    updated comments

--- PJ5/main.c
+++ PJ5/main.c
@@ -1,6 +1,17 @@
 #include <pthread.h>
 #include <stdio.h>
 
+/**
+ * Jecsan Blanco
+ * 2018FA OP SYSTEMS & NETWRK (CS-3352-01)
+ * Programming Assignment #05
+ *
+ * Write a C Linux program to calculate the sum and maximum of two inputed
+ * numbers. Two inputted numbers should be typed in from the keyboard.
+ * On thread will calculate the sum of the two inputs. The other thread
+ * will find the maximum value of the inputted numbers.
+ */
+
 struct SumNums {
     int a;
     int b;
@@ -8,12 +19,16 @@ struct SumNums {
     int max;
 };
 
+/* this function is run by the second thread
+ * gets the max(a,b) stored back in the the max var in struct*/
+
 void *max(void *vals) {
     struct SumNums *nums = (struct SumNums *) vals;
     nums->max = (nums->a > nums->b) ? nums->a : nums->b;
 }
 
-/* this function is run by the second thread */
+/* this function is run by the first thread
+* gets the sum( a,b) stored back in the the sum var in struct*/
 void *sum(void *vals) {
     /* increment x to 100 */
     struct SumNums *nums = (struct SumNums *) vals;
@@ -24,9 +39,10 @@ void *sum(void *vals) {
 int main() {
     int input[3];
     printf("Enter the first number: ");
-    scanf("%d",&input[0]);
+    scanf("%d", &input[0]);
     printf("Enter the second number: ");
-    scanf("%d",&input[1]);
+    scanf("%d", &input[1]);
+
     struct SumNums num = {input[0], input[1], 0};
     pthread_t sum_x_y_thread;
     /* create a second thread which executes inc_x(&x) */
